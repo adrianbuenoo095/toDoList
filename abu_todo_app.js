dbd3ea0cda1e1aed62fb609ece42ac4a9a3d560a
Adrian <adrian.bueno095@gmail.com>
2022-02-24 21:57:53 +0100

    -fixe problem local storage after refresh

--- abu_todo_app.js
+++ abu_todo_app.js
@@ -4,92 +4,90 @@
 // * Add a list of todos for example for work or school.
 // * Resposive for web and mobile
 // * Add a search bar
-(() => {
-    "user strict";
-    const todoInput = document.querySelector(".inputIteam");
-    const todoButton = document.querySelector(".todo-button");
-    const todoList = document.querySelector(".todo-list")
-    todoButton.addEventListener("click", addNewItem);
 
-    let deleteTrash = document.querySelectorAll(".fas.fa-trash")
-    if (deleteTrash.length === 0) return;
-    deleteTrash.forEach(element => {
-        element.addEventListener("click", deleteItem);
-    });
-
-    //#region Delete item from list 
-    function deleteItem(event) {
-        const item = todoList.children;
-        for (let i = 0; i < item.length; i++) {
-            if (item[i].contains(event.target)) {
-                todoList.removeChild(item[i]);
+        let todos =  JSON.parse(localStorage.getItem("todos")) || [];
+        const todoInput = document.querySelector(".inputIteam");
+        const todoButton = document.querySelector(".todo-button");
+        const todoList = document.querySelector(".todo-list");
+        
+        //Event Listener
+        // let deleteTrash = document.querySelectorAll(".fas.fa-trash");
+        todoList.addEventListener("click", deleteItem);
+        todoButton.addEventListener("click", addNewItem);
+            
+            //#region Delete item from list 
+            function deleteItem(event) {
+                const item = todoList.children;
+                for (let i = 0; i < item.length; i++) {
+                    if (item[i].contains(event.target)) {
+                        todoList.removeChild(item[i]);
+                    }
+                }
             }
-        }
-    }
-    //#endregion
-
-    function addNewItem(event) {
-        debugger;
-        event.preventDefault();
-        let userInputToDO = getItemTemplate(todoInput.value);
-
-        let newtodoButton = todoList.insertAdjacentHTML("beforeend", userInputToDO);
-        saveToDosToLocalStorage(newtodoButton);
-    }
-    //TODO: Save todos to local storage
-    //TODO: Set todos to local storage {https://thecodingpie.medium.com/how-to-build-a-todo-list-app-with-javascript-and-local-storage-a884f4ea3ec}
-    function saveToDosToLocalStorage(newTodo) {
-        //let toDosListLocalStorage = localStorage.getItem("todos");
-        //let todos = toDosListLocalStorage ? JSON.parse(toDosListLocalStorage) : [toDosListLocalStorage];
-        todos.push(newTodo);
-        localStorage.setItem("todos", JSON.stringify(todos));
-    }
-    /**
-     * 
-     * @param {string} todoInput
-     */
-    function getFromLocalStorage() {
-        const 
-
-    }
-
-    // function saveToLocalstorage(item){
-    //     debugger;
-    //     let todoItems = getLocalStorage();
-    //     let testing = getItemTemplate(todoButton.value);
-    //     if(testing.value === "") return;
-
-    //     todoItems.setItem(testing.value, testing.value);
-    //     Location.reload();
-    //     for (let i = 0; i < todoItems.length; i++) {
-    //         const key = todoItems.key(i);
-    //         const value = todoItems.getItem(key);
-
-    //         todoList.innerHTML += `<li>${value}</li>`;
-
-    //     }
-    //     localStorage.getItem("todoItems", JSON.stringify(todoItems));
-    // }
-
-
-    function getItemTemplate(contentValue) {
-
-        return `<li>
-                    <div class="content">
-                        <p>${contentValue}</p> 
-                    </div>  
-                    <div class="interactions" >
-                        <div class="completeTask">
-                            <button >
-                                <i class="fas fa-check"></i>
-                            </button>
-                        </div>
-                        <div id="deleteTask">
-                            <button" class="delete">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </div>
-                    </div>
-                </li>`;
-    }
-})();
+            //#endregion
+        
+            function addNewItem(event) {
+                debugger;
+                event.preventDefault();
+                let userInputToDO = getItemTemplate(todoInput.value);
+                if(userInputToDO === undefined){
+                    alert("Please enter a valid todo");
+                    return;
+                } 
+                //todoList.insertAdjacentHTML("beforeend", userInputToDO);
+                saveToDosToLocalStorage(userInputToDO);
+            }
+            //TODO: Save todos to local storage
+            //TODO: Set todos to local storage {https://thecodingpie.medium.com/how-to-build-a-todo-list-app-with-javascript-and-local-storage-a884f4ea3ec}
+            function saveToDosToLocalStorage(userInputToDO) {
+                debugger;
+                todos.push(userInputToDO);
+                localStorage.setItem("todos", JSON.stringify(todos));
+                todos.forEach(element => {
+                    if(element === null || element === undefined) return;
+                    todoList.insertAdjacentHTML("beforeend", element);
+                    return;
+                });
+            }
+        
+            // function saveToLocalstorage(item){
+            //     debugger;
+            //     let todoItems = getLocalStorage();
+            //     let testing = getItemTemplate(todoButton.value);
+            //     if(testing.value === "") return;
+        
+            //     todoItems.setItem(testing.value, testing.value);
+            //     Location.reload();
+            //     for (let i = 0; i < todoItems.length; i++) {
+            //         const key = todoItems.key(i);
+            //         const value = todoItems.getItem(key);
+        
+            //         todoList.innerHTML += `<li>${value}</li>`;
+        
+            //     }
+            //     localStorage.getItem("todoItems", JSON.stringify(todoItems));
+            // }
+        
+        
+            function getItemTemplate(contentValue) {
+                if (contentValue === "") return;
+                return `<li>
+                            <div class="content">
+                                <p>${contentValue}</p> 
+                            </div>  
+                            <div class="interactions" >
+                                <div class="completeTask">
+                                    <button >
+                                        <i class="fas fa-check"></i>
+                                    </button>
+                                </div>
+                                <div id="deleteTask">
+                                    <button" class="delete">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </li>`;
+        
+            }
+            saveToDosToLocalStorage();
