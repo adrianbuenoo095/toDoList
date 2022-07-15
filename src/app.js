10cb54d2461eab5d4a62d5d613c026897dc2fca0
Adrian.Bueno <adrian.bueno@connectiv.de>
2022-07-15 10:42:52 +0200

    I am not sure what i did, may god bless you

--- src/app.js
+++ src/app.js
@@ -1,3 +1,53 @@
-'use strict';
+function* generateSequence() {
+    yield "Hello world";
+    yield 2;
+    // yield* generatorTester();
+    yield normalFunction();
+    return 3;
+}
 
-console.log('Hello world');
+function* generatorTester() {
+    yield "I am generator";
+
+}
+// let one = generateSequence;
+// one.next().value;
+
+// for (let value of generateSequence()) {
+//     console.log(value);
+// }
+
+function normalFunction() {
+    // for (const sequence of generateSequence()) {
+    //     console.log(sequence);
+    // }
+}
+
+
+
+function* generatorForLoop(num) {
+    for (let i = 0; i < num; i += 1) {
+        yield console.log(i);
+    }
+}
+
+// const genForLoop = generatorForLoop(5);
+
+// genForLoop.next(); // first console.log - 0
+// genForLoop.next(); // 1
+// genForLoop.next(); // 2
+// genForLoop.next(); // 3
+// genForLoop.next(); // 4
+
+
+
+const handleClick = (e) => {
+    const offClick = (evt) => {
+        if (e !== evt) {
+            menu.classList.toggle(OPEN_CLASS)
+            document.removeEventListener("click", offClick)
+        }
+    }
+
+
+}
