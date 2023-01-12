1769f4aa7dc44998347bfb3080931d219fc7a5c2
Adrian Bueno <Adrian.Bueno@connectiv.local>
2023-01-12 14:39:55 +0100

    add new file

---
+++ src/test.js
@@ -0,0 +1,16 @@
+class Dictionary {
+    constructor(wordsArray) {
+        this.dict = wordsArray;
+    }
+
+    isInDict(word) {
+        return this.dict.some((dictWird) => {
+            const regexTemplete = word.replaceAll('*', '.');
+            const regex = new RegExp(`^${regexTemplete}`);
+            return regex.test(dictWird);
+        })
+    }
+}
+
+const test = new Dictionary(['cat', 'car', 'bar'])
+console.log(test.isInDict('cat'));
