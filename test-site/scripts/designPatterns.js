93237341209ed9f8e9f0bf6dfa541b5c2cc241e7
Adrian <adrianbueno095@gmail.com>
2023-01-26 13:19:26 +0100

    refactor code and fix typo

---
+++ test-site/scripts/designPatterns.js
@@ -0,0 +1,32 @@
+var printer = (function () {
+  var printerInstance;
+
+  function create() {
+    function print() {}
+
+    function turnOn() {}
+    return {
+      //public + private states and behaviors
+      print: print,
+      turnOn: turnOn,
+    };
+  }
+
+  return {
+    getInstance: function () {
+      if (!printerInstance) {
+        printerInstance = create();
+      }
+      return printerInstance;
+    },
+  };
+
+  function Singleton() {
+    if (!printerIntancance) {
+      printerIntancance = intialize();
+    }
+  }
+})(); //invocked
+
+var officePriner = printer.getInstance();
+console.log(officePriner);
