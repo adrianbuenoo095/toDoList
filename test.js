edefa027eb496fb204d9eff0ffb94a218f7d9fc5
Adrian <adrianbueno095@gmail.com>
2023-01-28 15:07:06 +0100

    refactor code

--- test.js
+++ test.js
@@ -1,23 +1,22 @@
-// https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript/
-
 function solution(number) {
-  let sum = [];
+  let arrayOfMultiplesOfThreeFive = [];
+  let sumOfAllNumbersThreeAndFive;
+
   for (let i = 0; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
-      isNumberNegative(i)
-      sum.join(i)
+      arrayOfMultiplesOfThreeFive.push(i);
     }
   }
 
-  let sumTest = sum.reduce((preValue, currentValye) => preValue + currentValye);
-  return sumTest;
+  sumOfAllNumbersThreeAndFive = arrayOfMultiplesOfThreeFive.reduce(
+    (preValue, currentValye) => preValue + currentValye,
+    0
+  );
 
+  return sumOfAllNumbersThreeAndFive;
 }
 
 function isNumberNegative(number) {
   if (number > 0) return 0;
 }
-
-
-let test = solution(10);
-console.log(test);
+ 
