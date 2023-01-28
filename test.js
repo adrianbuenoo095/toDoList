21a18186006b015bed48d9a8a2774963f06089cb
Adrian <adrianbueno095@gmail.com>
2023-01-28 14:44:43 +0100

    refactor code

--- test.js
+++ test.js
@@ -3,13 +3,21 @@
 function solution(number) {
   let sum = [];
   for (let i = 0; i < number; i++) {
-    console.log(`hello this is number ${i}`);
     if (i % 3 === 0 || i % 5 === 0) {
-      console.log(` This is the second number${i}`);
-      sum.join(i);
+      isNumberNegative(i)
+      sum.join(i)
     }
   }
+
+  let sumTest = sum.reduce((preValue, currentValye) => preValue + currentValye);
+  return sumTest;
+
 }
+
+function isNumberNegative(number) {
+  if (number > 0) return 0;
+}
+
 
 let test = solution(10);
 console.log(test);
